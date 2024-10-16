48819621e85755db39ad4a5216b54548560620f5
thisorself <reservposhtaclassroom@gmail.com>
2024-10-16 21:50:22 +0300

    edit

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
+for i in range(0, 10):
+    print("Hello World!")
 
